f0cfd70d8cae2264c7845570cbb3597093da5b01
jon.turney@dronecode.org.uk <jon.turney@dronecode.org.uk@dccc4ef9-3752-0410-9079-47cdceed9760>
2008-08-21 22:12:21 +0000

    Add back support for non-widechar Winamp interface
    git-svn-id: http://pidgin-musictracker.googlecode.com/svn/trunk@104 dccc4ef9-3752-0410-9079-47cdceed9760

--- src/winamp.c
+++ src/winamp.c
@@ -6,7 +6,7 @@
 static HWND hWnd;
 static HANDLE hProcess;
 
-void winamp_get_w(const wchar_t *filename, const wchar_t *key, char *dest)
+gboolean winamp_get_w(const wchar_t *filename, const wchar_t *key, char *dest)
 {
 	// Allocate memory inside Winamp's address space to exchange data with it
 	char *winamp_info = VirtualAllocEx(hProcess, NULL, 4096, MEM_COMMIT, PAGE_READWRITE);
@@ -24,15 +24,46 @@ void winamp_get_w(const wchar_t *filename, const wchar_t *key, char *dest)
 
         WriteProcessMemory(hProcess, winamp_filename, filename, sizeof(wchar_t)*(wcslen(filename)+1), NULL);
 	WriteProcessMemory(hProcess, winamp_key, key, sizeof(wchar_t)*(wcslen(key)+1), NULL);
-	SendMessage(hWnd, WM_WA_IPC, (WPARAM)winamp_info, IPC_GET_EXTENDED_FILE_INFOW);
+	int rc = SendMessage(hWnd, WM_WA_IPC, (WPARAM)winamp_info, IPC_GET_EXTENDED_FILE_INFOW);
 
 	SIZE_T bytesRead;
         wchar_t wdest[STRLEN];
-	int rc = ReadProcessMemory(hProcess, winamp_value, wdest, STRLEN-1, &bytesRead);
+	ReadProcessMemory(hProcess, winamp_value, wdest, STRLEN-1, &bytesRead);
 	wdest[bytesRead] = 0;
 
         WideCharToMultiByte(CP_UTF8, 0, wdest, -1, dest, STRLEN, NULL, NULL);
  	trace("Got info '%s', return value %d", dest, rc);
+
+        return (rc != 1);
+}
+
+gboolean winamp_get(const char *filename, const char *key, char *dest)
+{
+        // Allocate memory inside Winamp's address space to exchange data with it
+        char *winamp_info = VirtualAllocEx(hProcess, NULL, 4096, MEM_COMMIT, PAGE_READWRITE);
+        char *winamp_filename = (char*)(winamp_info+1024);
+        char *winamp_key = (char*)(winamp_info+2048);
+        char *winamp_value = (char*)(winamp_info+3072);
+
+        // Setup structure with pointers into Winamp address space and store it into that space too
+        extendedFileInfoStruct info;
+        info.filename = winamp_filename;
+        info.metadata = winamp_key;
+        info.ret = winamp_value;        
+        info.retlen = 1024;
+        WriteProcessMemory(hProcess, winamp_info, &info, sizeof(info), NULL);
+
+        WriteProcessMemory(hProcess, winamp_filename, filename, strlen(filename)+1, NULL);
+        WriteProcessMemory(hProcess, winamp_key, key, strlen(key)+1, NULL);
+        int rc = SendMessage(hWnd, WM_WA_IPC, (WPARAM)winamp_info, IPC_GET_EXTENDED_FILE_INFO);
+
+        SIZE_T bytesRead;
+        ReadProcessMemory(hProcess, winamp_value, dest, STRLEN-1, &bytesRead);
+        dest[bytesRead] = 0;
+
+        trace("Got info for key '%s' is '%s', return value %d", key, dest, rc);
+
+        return (rc != 1);
 }
 
 gboolean get_winamp_info(struct TrackInfo* ti)
@@ -61,16 +92,34 @@ gboolean get_winamp_info(struct TrackInfo* ti)
 	ti->currentSecs = SendMessage(hWnd, WM_WA_IPC, 0, IPC_GETOUTPUTTIME)/1000;
 
 	int position = SendMessage(hWnd, WM_WA_IPC, 0, IPC_GETLISTPOS);
-        wchar_t wfilename[512];
-	LPCVOID address = (LPCVOID) SendMessage(hWnd, WM_WA_IPC, position, IPC_GETPLAYLISTFILEW);
-	ReadProcessMemory(hProcess, address, wfilename, 512, 0);
-        char *f = wchar_to_utf8(wfilename);
-	trace("Filename: %s", f);
-        free(f);
 
-        winamp_get_w(wfilename, L"ALBUM", ti->album);
-        winamp_get_w(wfilename, L"ARTIST", ti->artist);
-        winamp_get_w(wfilename, L"TITLE", ti->track);
+        // first try wchar interface
+	LPCVOID address = (LPCVOID) SendMessage(hWnd, WM_WA_IPC, position, IPC_GETPLAYLISTFILEW);
+        if ((unsigned int)address > 1)
+          {
+            wchar_t wfilename[512];
+            ReadProcessMemory(hProcess, address, wfilename, 512, 0);
+            char *f = wchar_to_utf8(wfilename);
+            trace("Filename(widechar): %s", f);
+            free(f);
+
+            winamp_get_w(wfilename, L"ALBUM", ti->album);
+            winamp_get_w(wfilename, L"ARTIST", ti->artist);
+            winamp_get_w(wfilename, L"TITLE", ti->track);
+          }
+        else
+          {
+            // if that fails, fall back to char interface
+            // (this is not preferred as it cannot support east asian characters)
+            address = (LPCVOID) SendMessage(hWnd, WM_WA_IPC, position, IPC_GETPLAYLISTFILE);
+            char filename[512];
+            ReadProcessMemory(hProcess, address, filename, 512, 0);
+            trace("Filename: %s", filename);
+
+            winamp_get(filename, "ALBUM", ti->album);
+            winamp_get(filename, "ARTIST", ti->artist);
+            winamp_get(filename, "TITLE", ti->track);
+          }
 
         // if these are all empty, which seems to happen when listening to a stream, try something cruder
         // XXX: really should try to work out how to get winamp to resolve it's tag %streamtitle% for us...
@@ -78,7 +127,7 @@ gboolean get_winamp_info(struct TrackInfo* ti)
           {
             char *title = GetWindowTitleUtf8(hWnd);
             pcre *re;
-            re = regex("\\d*\\. (.*) - Winamp", 0);
+            re = regex("^\\d*\\. (.*?)(?: - Winamp|)$", 0);
             capture(re, title, strlen(title), ti->track);
             pcre_free(re);
             free(title);
