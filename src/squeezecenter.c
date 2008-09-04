9d1c0f6817fcf4b6281ca2aedb829ed392902bc4
jon.turney@dronecode.org.uk <jon.turney@dronecode.org.uk@dccc4ef9-3752-0410-9079-47cdceed9760>
2008-09-04 14:35:40 +0000

    Add GPL license, fix a typo
    git-svn-id: http://pidgin-musictracker.googlecode.com/svn/trunk@127 dccc4ef9-3752-0410-9079-47cdceed9760

--- src/squeezecenter.c
+++ src/squeezecenter.c
@@ -1,3 +1,27 @@
+/*
+ * musictracker
+ * squeezecenter.c
+ * retrieve track info from SqueezeCenter/SlimServer
+ *
+ * Copyright (C) 2008, Phillip Camp <phillip.camp@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02111-1301, USA.
+ *
+ */
+
 #include "musictracker.h"
 #include "utils.h"
 #include "glib.h"
@@ -27,7 +51,7 @@
 #define SQUEEZECENTER_PLAYER_BUFFER 20
 #define SQUEEZECENTER_TI_STRING STRLEN
 
-#define SQUEEZECENTER_ADVICE "Advice:-\n'#' Find playing player\n'*' Find powered player\n'id/name' prefix with ! to ignore if not playing\n\n Port Defaults to 9090, Multiple servers accepted delimeted by ','"
+#define SQUEEZECENTER_ADVICE "Advice:-\n'#' Find playing player\n'*' Find powered player\n'id/name' prefix with ! to ignore if not playing\n\n Port Defaults to 9090, Multiple servers accepted delimited by ','"
 
 typedef struct _squeezecenter_trackinfo {
     char title[SQUEEZECENTER_TI_STRING];
