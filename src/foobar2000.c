ba7acd4bd23e7c7dcc12dbb21fbb4a40dd084852
jon.turney@dronecode.org.uk <jon.turney@dronecode.org.uk@dccc4ef9-3752-0410-9079-47cdceed9760>
2008-12-05 17:56:03 +0000

    Added reported classname for Foobar2000 ColumnUI
    git-svn-id: http://pidgin-musictracker.googlecode.com/svn/trunk@201 dccc4ef9-3752-0410-9079-47cdceed9760

--- src/foobar2000.c
+++ src/foobar2000.c
@@ -11,6 +11,8 @@ get_foobar2000_info(struct TrackInfo* ti)
 		mainWindow = FindWindow("{DA7CD0DE-1602-45e6-89A1-C2CA151E008E}", NULL);
 	if (!mainWindow)
 		mainWindow = FindWindow("{97E27FAA-C0B3-4b8e-A693-ED7881E99FC1}", NULL); // Foobar 0.9.5.3 
+	if (!mainWindow)
+		mainWindow = FindWindow("{E7076D1C-A7BF-4f39-B771-BCBE88F2A2A8}", NULL); // Foobar Columns UI
 	if (!mainWindow) {
 		trace("Failed to find foobar2000 window. Assuming player is off");
 		ti->status = STATUS_OFF;
